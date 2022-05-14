6d861df7b00583ab388cb9e46625554147965cb7
dennyzain <abbasdenny24@gmail.com>
2022-05-14 23:14:57 +0700

    selection sort

--- src/bubble-sort.ts
+++ src/bubble-sort.ts
@@ -1,10 +1,10 @@
-export const bubbleSort = (input: Array<number>) => {
+export const bubbleSort = (input: Array<number>): Array<number> => {
   let unorderNum = input.length - 1;
   while (unorderNum > 0) {
     for (let i = 0; i < unorderNum; i++) {
       if (input[i] > input[i + 1]) {
         [input[i], input[i + 1]] = [input[i + 1], input[i]];
-      }
+      } // ! STILL WRONG
     }
     unorderNum -= 1;
   }
