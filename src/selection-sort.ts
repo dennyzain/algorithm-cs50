aaf73dc14a08988677e95a9025898fb70166f19b
dennyzain <abbasdenny24@gmail.com>
2022-05-15 00:52:59 +0700

    ASCII, binary, decimal-system

--- src/selection-sort.ts
+++
@@ -1,8 +0,0 @@
-export const selectionSort = (input: Array<number>): Array<number> => {
-  let lowestNum: number = input[0];
-  for (let i = 0; i < input.length; i++) {
-    if (input[i] > lowestNum) {
-      lowestNum = input[i];
-    }
-  }
-};
--- tests/selection-sort.test.ts
+++
@@ -1,6 +0,0 @@
-import { selectionSort } from '../src/selection-sort';
-
-it('should sorting array [3, 2] to be [2, 3]', () => {
-  const input = [3, 2];
-  expect(selectionSort(input)).toStrictEqual([2, 3]);
-});
